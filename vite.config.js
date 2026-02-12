6a25f93bd5d87444a0e659a7b1a861cab78d9713
Joshhsieh <halu_9@hotmail.com>
2026-02-12 16:11:01 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Day43-React-1-React-Router-joshhsieh/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Day44-React-2-React-Router-joshhsieh/' : '/',
   plugins: [react()],
 })
